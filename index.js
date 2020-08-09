a24f92b1519fa74a1cf814d044354e47f16f9ebc
Yuvraaj Narula <49155095+yuvraajnarula@users.noreply.github.com>
2020-08-09 20:43:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+	var i = 0;
+		var tag = document.getElementById("text");
+		var html = document.getElementById("text").innerHTML;
+		var attr = tag.setAttribute("data", html);
+		var txt = tag.getAttribute("data");
+		var speed = 50;
+
+		function typeWriter() {
+		  if (i <= txt.length) {
+		    document.getElementById("text").innerHTML = txt.slice(0 , i + 1);
+		    i++;
+		    setTimeout(typeWriter, speed);
+		  }
+		    //console.log(document.getElementById("text").innerHTML);
+		}
+
+typeWriter();
+const header = document.querySelector("header");
+window.addEventListener("scroll",()=>{
+  header.classList.toggle("change", window.scrollY > 0)
+})

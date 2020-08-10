68afceda3916eaba2f4a234ec7f4c66ee34686c3
Yuvraaj Narula <49155095+yuvraajnarula@users.noreply.github.com>
2020-08-10 09:23:35 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-	var i = 0;
-		var tag = document.getElementById("text");
-		var html = document.getElementById("text").innerHTML;
-		var attr = tag.setAttribute("data", html);
-		var txt = tag.getAttribute("data");
-		var speed = 50;
+var i = 0;
+var tag = document.getElementById("text");
+var html = document.getElementById("text").innerHTML;
+var attr = tag.setAttribute("data", html);
+var txt = tag.getAttribute("data");
+var speed = 50;
 
-		function typeWriter() {
-		  if (i <= txt.length) {
-		    document.getElementById("text").innerHTML = txt.slice(0 , i + 1);
-		    i++;
-		    setTimeout(typeWriter, speed);
-		  }
-		    //console.log(document.getElementById("text").innerHTML);
-		}
+function typeWriter() {
+  if (i <= txt.length) {
+    document.getElementById("text").innerHTML = txt.slice(0 , i + 1);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+    //console.log(document.getElementById("text").innerHTML);
+}
 
 typeWriter();
-const header = document.querySelector("header");
+const header = document.querySelector(".header");
 window.addEventListener("scroll",()=>{
-  header.classList.toggle("change", window.scrollY > 0)
+header.classList.toggle("change", window.scrollY > 0)
 })
 
 const backToTopButton = document.querySelector("#back-to-top-btn");
@@ -25,22 +25,22 @@ const backToTopButton = document.querySelector("#back-to-top-btn");
 window.addEventListener("scroll", scrollFunction);
 
 function scrollFunction() {
-  if (window.pageYOffset > 300) { // Show backToTopButton
-    if(!backToTopButton.classList.contains("btnEntrance")) {
-      backToTopButton.classList.remove("btnExit");
-      backToTopButton.classList.add("btnEntrance");
-      backToTopButton.style.display = "block";
-    }
-  }
-  else { // Hide backToTopButton
-    if(backToTopButton.classList.contains("btnEntrance")) {
-      backToTopButton.classList.remove("btnEntrance");
-      backToTopButton.classList.add("btnExit");
-      setTimeout(function() {
-        backToTopButton.style.display = "none";
-      }, 250);
-    }
-  }
+if (window.pageYOffset > 300) { // Show backToTopButton
+if(!backToTopButton.classList.contains("btnEntrance")) {
+  backToTopButton.classList.remove("btnExit");
+  backToTopButton.classList.add("btnEntrance");
+  backToTopButton.style.display = "block";
+}
+}
+else { // Hide backToTopButton
+if(backToTopButton.classList.contains("btnEntrance")) {
+  backToTopButton.classList.remove("btnEntrance");
+  backToTopButton.classList.add("btnExit");
+  setTimeout(function() {
+    backToTopButton.style.display = "none";
+  }, 250);
+}
+}
 }
 
 backToTopButton.addEventListener("click", smoothScrollBackToTop);
@@ -50,25 +50,25 @@ backToTopButton.addEventListener("click", smoothScrollBackToTop);
 // }
 
 function smoothScrollBackToTop() {
-  const targetPosition = 0;
-  const startPosition = window.pageYOffset;
-  const distance = targetPosition - startPosition;
-  const duration = 750;
-  let start = null;
-  
-  window.requestAnimationFrame(step);
+const targetPosition = 0;
+const startPosition = window.pageYOffset;
+const distance = targetPosition - startPosition;
+const duration = 750;
+let start = null;
 
-  function step(timestamp) {
-    if (!start) start = timestamp;
-    const progress = timestamp - start;
-    window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
-    if (progress < duration) window.requestAnimationFrame(step);
-  }
+window.requestAnimationFrame(step);
+
+function step(timestamp) {
+if (!start) start = timestamp;
+const progress = timestamp - start;
+window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+if (progress < duration) window.requestAnimationFrame(step);
+}
 }
 
 function easeInOutCubic(t, b, c, d) {
-	t /= d/2;
-	if (t < 1) return c/2*t*t*t + b;
-	t -= 2;
-	return c/2*(t*t*t + 2) + b;
+t /= d/2;
+if (t < 1) return c/2*t*t*t + b;
+t -= 2;
+return c/2*(t*t*t + 2) + b;
 };
